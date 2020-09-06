1d08035c11c8dcaa2e5d1d69c05f444acdeb16f1
Craig Collis-McCann <craig@cmccanngbr.co.uk>
2020-09-06 15:41:49 +0100

    Refactored as per code review (moved more logic to methods)

--- RB101/lesson2/rock_paper_scissors_bonus_features.rb
+++ RB101/lesson2/rock_paper_scissors_bonus_features.rb
@@ -1,3 +1,4 @@
+#### GAME VARIABLES ####
 VALID_CHOICES = ['r', 'p', 'sc', 'l', 'sp']
 
 WINNING_COMBINATIONS = {
@@ -21,12 +22,6 @@ Lizard eats Paper
 Paper disproves Spock
 Spock vaporizes Rock
 (and as it always has) Rock crushes Scissors
-Please choose from:
-Rock = r
-Paper = p
-Scissors = sc
-Lizard = l
-Spock = sp
 MSG
 
 score_board = {
@@ -34,12 +29,50 @@ score_board = {
   computer: 0
 }
 
+#### HELPER FUNCTIONS ####
 def prompt(message)
   puts("=> #{message}")
 end
 
 def win?(first, second)
   WINNING_COMBINATIONS[first].include?(second)
+end
+
+#### MAIN FUNCTIONS ####
+def reset_score_board(score_board)
+  score_board[:player] = 0
+  score_board[:computer] = 0
+end
+
+def get_user_choice
+  choice = ''
+  loop do
+    prompt('Choose: r - rock, p - paper, sc - scissors, sp - spock, l -lizard')
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice...")
+    end
+  end
+  choice
+end
+
+def get_computer_choice
+  VALID_CHOICES.sample
+end
+
+def track_score(user_choice, computer_choice, score_board)
+  if win?(user_choice, computer_choice)
+    score_board[:player] += 1
+  elsif win?(computer_choice, user_choice)
+    score_board[:computer] += 1
+  end
+end
+
+def display_choices(user_choice, computer_choice)
+  "You chose #{user_choice}, the computer chose #{computer_choice}"
 end
 
 def display_result(player, computer)
@@ -52,52 +85,43 @@ def display_result(player, computer)
   end
 end
 
-def track_score(player, computer, score_board)
-  if win?(player, computer)
-    score_board[:player] += 1
-  elsif win?(computer, player)
-    score_board[:computer] += 1
-  end
-end
-
 def display_score(score_board)
   "Player = #{score_board[:player]} : Computer = #{score_board[:computer]}"
 end
 
+def clear_screen
+  sleep(2)
+  system('clear')
+end
+
+def winner?(score_board)
+  score_board[:player] == 5 || score_board[:computer] == 5
+end
+
+def play_again?
+  prompt('Do you want to play again? Please enter y/yes to begin')
+  answer = gets.chomp.downcase
+  answer == 'y' || answer == 'yes' ? true : false
+end
+
+#### MAIN GAME ####
 prompt(WELCOME)
 
 loop do
+  reset_score_board(score_board)
   loop do
-    choice = ''
-    loop do
-      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-      choice = gets.chomp
+    user_choice = get_user_choice
+    computer_choice = get_computer_choice
 
-      if VALID_CHOICES.include?(choice)
-        break
-      else
-        prompt("That's not a valid choice...")
-      end
-    end
+    track_score(user_choice, computer_choice, score_board)
 
-    computer_choice = VALID_CHOICES.sample
-
-    puts("You chose #{choice}, the computer chose #{computer_choice}")
-
-    track_score(choice, computer_choice, score_board)
-    prompt(display_result(choice, computer_choice))
+    prompt(display_choices(user_choice, computer_choice))
+    prompt(display_result(user_choice, computer_choice))
     prompt(display_score(score_board))
-    break if score_board[:player] == 5 || score_board[:computer] == 5
+    clear_screen
+    break if winner?(score_board)
   end
-
-  prompt('Do you want to play again?')
-  answer = gets.chomp
-  if answer.downcase.start_with?('y')
-    score_board[:player] = 0
-    score_board[:computer] = 0
-  else
-    break
-  end
+  break unless play_again?
 end
 
 prompt('Thanks for playing, good bye!')
